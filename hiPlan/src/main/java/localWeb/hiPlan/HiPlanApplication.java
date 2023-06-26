f137ac2f5c28f8cf519486ae8f7f1989ddf455db
GoGangH <rhrkdgus1020@naver.com>
2023-06-26 21:44:52 +0900

    일정 관리 프로젝트 생성

---
+++ hiPlan/src/main/java/localWeb/hiPlan/HiPlanApplication.java
@@ -0,0 +1,13 @@
+package localWeb.hiPlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiPlanApplication.class, args);
+	}
+
+}
---
+++ hiPlan/src/test/java/localWeb/hiPlan/HiPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package localWeb.hiPlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
